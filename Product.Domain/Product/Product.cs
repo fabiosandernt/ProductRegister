ea9bca4e92144d711351e1468d021c336aabb8c9
Fabio Sander Novaes Correa <fabio.ncorrea@al.infnet.edu.br>
2022-10-27 15:06:11 -0300

    Add project files.

---
+++ Product.Domain/Product/Product.cs
@@ -0,0 +1,16 @@
+﻿using Product.CrossCutting.Entity;
+
+
+namespace Product.Domain.Product
+{
+    public class Product: Entity<Guid>
+    {
+        public string Titulo { get; set; }
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public double Valor { get; set; }
+        public DateTime DataCriacao { get; set; } 
+        public DateTime DataAtualizacao { get; set; }
+    
+    }
+}
